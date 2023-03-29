d0819d619e517b41ab8c0747f19099692838fc50
johnmichael <withyou746@gmail.com>
2023-03-29 18:20:06 +0000

    metamask completed

--- src/pages/Pair/index.jsx
+++ src/pages/Pair/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Container, TextField, Link, Modal, Typography, Button } from '@mui/material';
 import { SmallIcon } from './style';
 import { useMetaMask } from "metamask-react";
@@ -7,13 +7,24 @@ const ariaLabel = { 'aria-label': 'description' };
 
 const Pair = () => {
     const { status, connect, account, chainId, ethereum } = useMetaMask();
+    const [connectStatus, setConnectStatus] = useState("");
+    const [inputAddress, setInputAddress] = useState("");
 
     useEffect(() => {
-        console.log("status", status);
-        if (status === "connected") {
-            console.log("account", account);
+        if (status === "notConnected" || status === "initializing") {
+            setConnectStatus("Connect Wallet");
+        } else if (status === "connected") {
+            setConnectStatus(account);
+            setInputAddress(account);
+        }
+        else {
+            setConnectStatus(status);
         }
     }, [status])
+
+    const onChangeAddress = (e) => {
+        setInputAddress(e.target.value);
+    }
 
     return (
         <Container className='d-flex align-items-center justify-content-center'
@@ -88,6 +99,7 @@ const Pair = () => {
                     </ul>
                 </Typography>
                 <TextField placeholder="0x..." inputProps={ariaLabel} focused
+                    onChange={onChangeAddress}
                     sx={{
                         width: '100%',
                         backgroundColor: 'primary.dark',
@@ -106,6 +118,7 @@ const Pair = () => {
                             border: 'none'
                         }
                     }}
+                    value={inputAddress}
                 />
                 <Typography
                     className='text-center'
@@ -117,26 +130,100 @@ const Pair = () => {
                 >
                     e.g. 0x21A104A118e43BeE49206e3e8b115580234dD423
                 </Typography>
-                <Box className='d-flex align-items-center justify-content-center'>
-                    <Button variant="outlined"
-                        sx={{
-                            borderColor: 'primary.outline',
-                            borderRadius: '50px',
-                            color: 'primary.green',
-                            marginTop: '30px',
+                {!account ?
+                    <Box className='d-flex align-items-center justify-content-center'>
+                        <Button variant="outlined"
+                            sx={{
+                                borderColor: 'primary.outline',
+                                borderRadius: '50px',
+                                color: 'primary.green',
+                                marginTop: '30px',
 
-                            '&:hover': {
-                                backgroundColor: '#68d67c20'
-                            }
+                                '&:hover': {
+                                    backgroundColor: '#68d67c20'
+                                }
+                            }}
+                            onClick={connect}
+                        >
+                            {connectStatus}
+                        </Button>
+                    </Box>
+                    :
+                    <Box
+                        sx={{
+                            width: '100%',
+                            maxWidth: '600px',
+                            backgroundColor: 'primary.main',
+                            boxShadow: '0 5.40728px 10.8146px rgba(0,0,0,.3)',
+                            borderRadius: '20px',
+                            marginTop: '30px',
+                            padding: '20px'
                         }}
-                        onClick={connect}
                     >
-                        {/* {state.loading ? status : "Connect Wallet"} */}
-                        {status}
-                    </Button>
-            {/* <Button type="submit" label={state.loading ? status : "Login with Wallet"} disabled={(state.loading)} onClick={connect}/>
-            <div className="server-error">{state.error && state.error}</div> */}
-                </Box>
+                        <Typography
+                            color='primary.text'
+                            sx={{
+                                fontSize: '24px',
+                                marginBottom: '30px'
+                            }}
+                        >
+                            Pair found
+                        </Typography>
+                        <Box
+                            className='d-flex align-items-center justify-content-between mb-3 w-100'
+                        >
+                            <Box className='d-flex align-items-center'>
+                                <Box>
+                                    <SmallIcon
+                                        src="https://testnet.switch.mute.io/icon.png"
+                                        alt='Mute.Switch'
+                                        sx={{ borderRadius: '50%' }}
+                                    />
+                                    <SmallIcon
+                                        src="https://testnet.switch.mute.io/icon.png"
+                                        alt='Mute.Switch'
+                                        className='me-3'
+                                        sx={{ borderRadius: '50%' }}
+                                    />
+                                </Box>
+                                <Box className='d-flex align-items-center'>
+                                    <Typography
+                                        color='primary.text'
+                                        sx={{
+                                            fontSize: '16px',
+                                        }}
+                                    >
+                                        WETH / DEXT
+                                    </Typography>
+                                </Box>
+                            </Box>
+                            <Typography
+                                color='primary.text'
+                                sx={{
+                                    fontSize: '16px',
+                                }}
+                            >
+                                0
+                            </Typography>
+                        </Box>
+                        <Button
+                            className='w-100'
+                            sx={{
+                                height: '50px',
+                                backgroundColor: 'primary.green',
+                                borderRadius: '100px',
+                                color: 'white',
+                                marginTop: '20px',
+
+                                '&:hover': {
+                                    backgroundColor: '#68d67c5f'
+                                }
+                            }}
+                        >
+                            CONTINUE
+                        </Button>
+                    </Box>
+                }
             </Box>
         </Container>
     );
